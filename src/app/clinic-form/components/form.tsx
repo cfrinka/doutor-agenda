0774ef0f9977a7bd7e54a92292020f6420b613b3
Carlos Neto <c.frinkaneto@gmail.com>
2025-05-28 11:20:45 -0300

    feat: integrate Radix UI dialog and enhance dashboard session handling

---
+++ src/app/clinic-form/components/form.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { createClinic } from "@/app/actions/create-clinic";
+import { Button } from "@/components/ui/button";
+import { DialogFooter } from "@/components/ui/dialog";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+  Form,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+
+const clinicFormSchema = z.object({
+  name: z.string().trim().min(1, { message: "Nome é obrigatório" }).max(50),
+});
+
+const ClinicForm = () => {
+  const form = useForm<z.infer<typeof clinicFormSchema>>({
+    resolver: zodResolver(clinicFormSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof clinicFormSchema>) {
+    try {
+      await createClinic(values.name);
+      toast.success("Clínica criada com sucesso");
+      form.reset();
+    } catch (error) {
+      if (isRedirectError(error)) {
+        return;
+      }
+      console.error(error);
+      toast.error("Error ao criar a clínica");
+    }
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nome</FormLabel>
+                <FormControl>
+                  <Input placeholder="Digite o nome da clínica" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <DialogFooter>
+            <Button type="submit" disabled={form.formState.isSubmitting}>
+              {form.formState.isSubmitting && (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Criar clínica
+            </Button>
+          </DialogFooter>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default ClinicForm;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,9 @@ import { headers } from "next/headers";
 import React from "react";
 import SignOutButton from "./components/sign-out-button";
 import { redirect } from "next/navigation";
+import { db } from "@/db";
+import { eq } from "drizzle-orm";
+import { usersToClinicsTable } from "@/db/schema";
 
 const DashboardPage = async () => {
   const session = await auth.api.getSession({
@@ -13,6 +16,13 @@ const DashboardPage = async () => {
     redirect("/authentication");
   }
 
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
+  }
   return (
     <div>
       <h1>Dashboard</h1>
