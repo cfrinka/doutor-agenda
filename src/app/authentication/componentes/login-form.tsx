c421f01775ab247cc22e495521b305b6aec534d1
Carlos Neto <c.frinkaneto@gmail.com>
2025-05-27 22:00:41 -0300

    chore: adds better-auth setup

--- src/app/authentication/componentes/login-form.tsx
+++ src/app/authentication/componentes/login-form.tsx
@@ -22,8 +22,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import z from "zod";
 
-const registerSchema = z.object({
-  name: z.string().trim().min(1, { message: "Nome é obrigatório" }),
+const loginSchema = z.object({
   email: z
     .string()
     .trim()
@@ -36,17 +35,16 @@ const registerSchema = z.object({
     .min(8, { message: "Senha deve ter pelo menos 8 caractéres" }),
 });
 
-const SignUpForm = () => {
-  const form = useForm<z.infer<typeof registerSchema>>({
-    resolver: zodResolver(registerSchema),
+const LoginForm = () => {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
     defaultValues: {
       email: "",
-      name: "",
       password: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof registerSchema>) {
+  function onSubmit(values: z.infer<typeof loginSchema>) {
     console.log(values);
   }
   return (
@@ -54,23 +52,12 @@ const SignUpForm = () => {
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
           <CardHeader>
-            <CardTitle>Criar conta</CardTitle>
-            <CardDescription>Crie uma conta para continuar</CardDescription>
+            <CardTitle>Entrar</CardTitle>
+            <CardDescription>
+              Digite suas credenciais para continuar
+            </CardDescription>
           </CardHeader>
           <CardContent className="space-y-4">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Nome Completo</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Digite seu nome completo" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
             <FormField
               control={form.control}
               name="email"
@@ -113,4 +100,4 @@ const SignUpForm = () => {
   );
 };
 
-export default SignUpForm;
+export default LoginForm;
--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,30 +1,6 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { z } from "zod";
 import SignUpForm from "./componentes/sign-up-form";
-
-const loginSchema = z.object({
-  email: z
-    .string()
-    .trim()
-    .email({ message: "Email é inválido" })
-    .min(1, { message: "Email é obrigatório" })
-    .max(50),
-  password: z
-    .string()
-    .trim()
-    .min(8, { message: "Senha deve ter pelo menos 8 caractéres" }),
-});
+import LoginForm from "./componentes/login-form";
 
 export default function AuthenticationPage() {
   return (
@@ -35,16 +11,7 @@ export default function AuthenticationPage() {
           <TabsTrigger value="register">Criar conta</TabsTrigger>
         </TabsList>
         <TabsContent value="login">
-          <Card>
-            <CardHeader>
-              <CardTitle>Login</CardTitle>
-              <CardDescription>Faça login para continuar</CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2"></CardContent>
-            <CardFooter>
-              <Button>Entrar</Button>
-            </CardFooter>
-          </Card>
+          <LoginForm />
         </TabsContent>
         <TabsContent value="register">
           <SignUpForm />
