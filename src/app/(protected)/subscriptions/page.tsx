fcb047196a66b628c9cbeaa1db1dab8f094de18c
Carlos Neto <c.frinkaneto@gmail.com>
2025-06-13 20:52:05 -0300

    feat: ensure stripe session is created

---
+++ src/app/(protected)/subscriptions/page.tsx
@@ -0,0 +1,43 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import {
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/_components/ui/page-container";
+import { auth } from "@/lib/auth";
+import { SubscriptionPlan } from "./_components/subscription-plan";
+
+const SubscriptionPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    redirect("/login");
+  }
+  if (!session.user.clinic) {
+    redirect("/clinic-form");
+  }
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Assinatura</PageTitle>
+          <PageDescription>Gerencie a sua assinatura.</PageDescription>
+        </PageHeaderContent>
+      </PageHeader>
+      <PageContent>
+        <SubscriptionPlan
+          className="w-[350px]"
+          userEmail={session.user.email}
+        />
+      </PageContent>
+    </PageContainer>
+  );
+};
+
+export default SubscriptionPage;
